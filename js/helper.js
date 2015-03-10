7270147189911a4098fcaae4f025ee7d8afcd021
Mike Zaret <michael.zaret@gmail.com>
2015-03-09 22:33:31 -0700

    Big Update of files with working version

--- js/helper.js
+++ js/helper.js
@@ -52,8 +52,8 @@ var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
+var HTMLonlineURL = '<br><a href=%data%>Class Link</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -65,7 +65,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -85,7 +85,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  // your code goes here
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
 });
 
 
@@ -170,7 +174,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +237,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,215 @@
+var skills = ["R","Python","SQL","Classification Algorithms"];
+var contacts = {
+	"email":"michael.zaret@gmail.com",
+	"mobile":"770-598-8538",
+	"twitter":"mzaret2000",
+	"github":"mzaret2000",
+	"location":"Oakland, CA"
+};
+
+var work = {
+	jobs:[
+	{"employer":"Vinyl Interactive",
+	"title":"Marketing Analyst",
+	"location":"San Francisco",
+	"dates":"2009-2011",
+	"description":"Build and maintain algorithms to rank leads for online education clients",
+	"url":"http://vinylinteractive.com/"
+	},
+	{"employer":"Zynga",
+	"title":"Analytics Manager",
+	"location":"Los Angeles",
+	"dates":"2011-2013",
+	"description":"Add quantitative insights and maintain dashboards for analytically driven game studios",
+	"url":"http://www.zynga.com"
+	},
+	{"employer":"Uber",
+	"title":"Operations Manager",
+	"location":"Chicago",
+	"dates":"2014-",
+	"description":"Develop Incentive Programs to keep partners engaged.Make sure team is making decisions informed by data.",
+	"url":"https://www.uber.com/"
+	}]
+};
+
+var projects = {
+	detail:[
+	{"title":"P1:Coffee Mug",
+	"dates":"January 2015",
+	"description":"Build a webpage from a mockup using boostrap framework.",
+	"image":"/Users/mikezaret/frontend-nanodegree-resume/images/mug.png"
+	},
+	{"title":"P2: Interactive Resume",
+	"dates":"February 2015",
+	"description":"Build an interactive webpage with JavaScript",
+	"image":"/Users/mikezaret/frontend-nanodegree-resume/images/p2.png"
+	}]
+};
+
+var bio = {
+	"name":"Michael Zaret",
+	"role":"Web Developer",
+	"welcomeMessage":"Hi Michael Zaret, and I am learning to become a Front-End Web Developer",
+	"biopic":"/Users/mikezaret/frontend-nanodegree-resume/images/biopic.png",
+	"contacts":contacts,
+	"skills":skills
+};
+
+var education = {
+	"schools":[
+	{"name":"Princeton University",
+	"location":"Princeton, New Jersey",
+	"degree":"BA",
+	"majors":["Economics","Finance Certificate"],
+	"dates":"2007",
+	"url":"http://www.princeton.edu"
+	}],
+	"onlineCourses":[
+	{"title":"JavaScript Basics",
+	"school":"Udacity",
+	"dates":"2015",
+	"url":"http://www.udacity.com/course/ud804"
+	},
+	{"title":"Intro to Computer Science",
+	"school":"Udacity",
+	"dates":"2013",
+	"url":"https://www.udacity.com/course/cs101"
+	},
+	{"title":"Build a Blog",
+	"school":"Udacity",
+	"dates":"2014",
+	"url":"https://www.udacity.com/course/cs253"
+	},
+	{"title":"Intro to Machine Learning",
+	"school":"Coursera",
+	"dates":"2012",
+	"url":"https://www.coursera.org/course/ml"
+	}]
+};
+
+contactWrap = function(id) {
+	var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+	$(id).append(formattedMobile);
+	var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+	$(id).append(formattedEmail);
+	var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+	$(id).append(formattedTwitter);
+	var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+	$(id).append(formattedGithub);
+	var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
+	$(id).append(formattedLocation);
+};
+
+bio.display = function() {
+	var formattedHeaderRole = HTMLheaderRole.replace("%data%",bio.role);
+	$("#header").prepend(formattedHeaderRole);
+	var formattedHeaderName = HTMLheaderName.replace("%data%",bio.name);
+	$("#header").prepend(formattedHeaderName);
+	contactWrap("#topContacts");
+	var formattedBiopic = HTMLbioPic.replace("%data%",bio.biopic);
+	$("#header").append(formattedBiopic)
+	var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
+	$("#header").append(formattedWelcomeMsg);
+	$("#header").append(HTMLskillsStart);
+	for (s in bio.skills) {
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[s]);
+		$("#skills").append(formattedSkill);
+	};
+};
+
+
+
+work.display = function() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		formattedEmployer = formattedEmployer.replace("#",work.jobs[job].url);
+		var formattedworkTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedworkLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+		var formattedworkDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+		var formattedworkDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+		$(".work-entry:last").append(formattedEmployer + "-" + formattedworkTitle);
+		$(".work-entry:last").append(formattedworkLocation);
+		$(".work-entry:last").append(formattedworkDates);
+		$(".work-entry:last").append(formattedworkDescription);
+	};
+};
+
+projects.display = function() {
+	for (p in projects.detail) {
+		$("#projects").append(HTMLprojectStart);
+		var formattedprojectTitle = HTMLprojectTitle.replace("%data%",projects.detail[p].title);
+		var formattedprojectDates = HTMLprojectDates.replace("%data%",projects.detail[p].dates);
+		var formattedprojectDescription = HTMLprojectDescription.replace("%data%",projects.detail[p].description);
+		var formattedprojectImage = HTMLprojectImage.replace("%data%",projects.detail[p].image);
+		$(".project-entry:last").append(formattedprojectTitle);
+		$(".project-entry:last").append(formattedprojectDates);
+		$(".project-entry:last").append(formattedprojectDescription);
+		$(".project-entry:last").append(formattedprojectImage);
+
+	};
+};
+
+education.display = function() {
+	for (univ in education.schools) {
+	$("#education").append(HTMLschoolStart);
+	var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[univ].name);
+	formattedSchoolName = formattedSchoolName .replace("#",education.schools[univ].url);
+	var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[univ].location);
+	var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[univ].degree);
+	var formattedSchoolMajor = HTMLschoolMajor.replace("%data%",education.schools[univ].majors);
+	var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[univ].dates);
+	var formattedSchool = formattedSchoolName + formattedSchoolDegree
+	$(".education-entry:last").append(formattedSchool);
+	$(".education-entry:last").append(formattedSchoolDates);
+	$(".education-entry:last").append(formattedSchoolLocation);
+	$(".education-entry:last").append(formattedSchoolMajor);
+	}
+	$("#education").append(HTMLonlineClasses);
+	for (course in education.onlineCourses) {
+		$("#education").append(HTMLschoolStart);
+		var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[course].title);
+		formattedOnlineTitle = formattedOnlineTitle.replace("#",education.onlineCourses[course].url);
+		var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[course].school);
+		var formattedOnlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses[course].dates);
+		var formattedOnlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[course].url);
+		var titlePlusSchool = formattedOnlineTitle + " - " + formattedOnlineSchool
+		$(".education-entry:last").append(titlePlusSchool);
+		$(".education-entry:last").append(formattedOnlineDates);
+		//$(".education-entry:last").append(formattedOnlineURL);
+	};
+};
+
+projects.display();
+work.display();
+bio.display();
+education.display();
+contactWrap("#footerContacts");
+
+$("#mapDiv").append(googleMap);
+
+$(document).ready(function(){
+    $("#workExperience").children("h2").click(function(){
+        $(".work-entry").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#education").children("h2").click(function(){
+        $(".education-entry").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#projects").children("h2").click(function(){
+        $(".project-entry").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#mapDiv").children("h2").click(function(){
+        $("#map").slideToggle();
+    });
+});
+
+
